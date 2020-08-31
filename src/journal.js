21a0c19f5cc38a57256c417fe9f5fa8bebdb6429
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-31 16:34:33 -0700

    Add functions to journal.js.  Add links to main.js

--- src/journal.js
+++ src/journal.js
@@ -4,7 +4,10 @@ function Journal(title, body) {
   this.entry = [];
 }
 
-Journal.prototype.addEntry = function (body) {
+let exampleJournal = New Journal("title","body")
+
+Journal.prototype.addEntry = function (title, body) {
+  this.title = title;
   let newEntry = body.split(" ");
   return Journal.entry.push(newEntry);
 };
@@ -13,3 +16,10 @@ Journal.prototype.getLength = function () {
   let arraylength = Journal.entry.length;
   return arraylength;
 };
+
+export default function (title, body, entry) {
+  this.title = title;
+  this.body = body;
+  this.entry = [];
+}
+git 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import "src/journal.js";
 
-$("form#journalcontainer).submit(function (event) {
+$("form#journalcontainer").submit(function (event) {
   const journaltitle = $("input#journaltitle").val();
   const journalbody = $("input#journalbody").val();
 
